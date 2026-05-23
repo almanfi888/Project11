e494b7812d84fde16e58d81794aa91bbc5512311
almanfi <aa19163278750@outlook.com>
2026-05-23 21:18:17 +0800

    2025.5.23.雨课堂第一题

--- Project11/FileName.cpp
+++ Project11/FileName.cpp
@@ -1,43 +1,73 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
-
-class Bclass {
-public:
-	Bclass(int i, int j) {
-		x = i;
-		y = j;
-	}
-
-	int fun() {
-		return 0;
-	}
-
-protected:
-	int x, y;
-};
-
-class Iclass : public Bclass {
+class person {
 private:
-	int z;
+
+	char* xm;
+	char* xb;
+	int age;
 public:
-	Iclass(int i, int j, int k) : Bclass(i, j) {
-		z = k;
+	void set_p() {
+		char name[20];
+		cout << "请输入姓名：";
+		cin >> name;
+		xm = new char[strlen(name) + 1];
+		strcpy_s(xm, strlen(name) + 1, name);
+		char sex[20];
+		cout << "请输入性别：";
+		cin >> sex;
+		xb = new char[strlen(sex) + 1];
+		strcpy_s(xb, strlen(sex) + 1, sex);
+		cout << "请输入年龄：";
+		cin >> age;
+	}
+	void display_p() {
+		cout << "姓名:" << xm << "性别：" << xb << "年龄:"<<age;
+	}
+	~person() {
+		delete[] xm;
+		delete[] xb;
 	}
 
-	int fun() {
-		return (x + y + z) / 3;
+};
+class student :public person {
+private:
+	int xuehao;
+	int banji;
+	char* zhuanye;
+	int rxcj;
+public:
+	void set_t() {
+		set_p();
+		cout << "请输入学号：";
+		cin >> xuehao;
+		cout << "请输入班级：";
+		cin >> banji;
+		char zy[20];
+		cout << "请输入专业：";
+		cin >> zy;
+		zhuanye = new char[strlen(zy) + 1];
+		strcpy_s(zhuanye, strlen(zy) + 1, zy);
+		cout << "请输入入学成绩：";
+		cin >> rxcj;
+	}
+	void display_s() {
+		display_p();
+		cout << "学号：" << xuehao << "班级：" << banji << "专业：" << zhuanye << "入学成绩：" << rxcj;
+	}
+	~student() {
+		delete[]zhuanye;
 	}
 };
-
-
 int main() {
-	Iclass obj(2, 4, 10);
-	Bclass p1 = obj;
+	student s;   
+	s.set_t(); 
 
-	cout << p1.fun() << endl;
+	s.display_s();
 
 	return 0;
 
-
-
 }
+
+
