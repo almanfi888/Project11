8f8ff97bd68ce437c20f5d4af65c7693d92b2897
almanfi <aa19163278750@outlook.com>
2026-05-29 16:26:42 +0800

    上次的第二题

--- Project11/FileName.cpp
+++ Project11/FileName.cpp
@@ -2,29 +2,31 @@
 #include <string>
 using namespace std;
 class person {
-private:
+protected:
 
 	char* xm;
 	char* xb;
 	int age;
 public:
-	void set_p() {
+	friend ostream& operator<<(ostream& out, person& s) {
+		out << "姓名:" << s.xm << "性别：" << s.xb << "年龄:" << s.age;
+		return out;
+	}
+	friend istream& operator>>(istream& in, person& s) {
 		char name[20];
 		cout << "请输入姓名：";
-		cin >> name;
-		xm = new char[strlen(name) + 1];
-		strcpy_s(xm, strlen(name) + 1, name);
-		char sex[20];
+		in >> name;
+		s.xm = new char[strlen(name) + 1];
+		strcpy_s(s.xm, strlen(name) + 1, name);		char sex[20];
 		cout << "请输入性别：";
-		cin >> sex;
-		xb = new char[strlen(sex) + 1];
-		strcpy_s(xb, strlen(sex) + 1, sex);
+		in >> sex;
+		s.xb = new char[strlen(sex) + 1];
+		strcpy_s(s.xb, strlen(sex) + 1, sex);
 		cout << "请输入年龄：";
-		cin >> age;
+		in >> s.age;
+		return in;
 	}
-	void display_p() {
-		cout << "姓名:" << xm << "性别：" << xb << "年龄:"<<age;
-	}
+
 	~person() {
 		delete[] xm;
 		delete[] xb;
@@ -38,34 +40,47 @@ private:
 	char* zhuanye;
 	int rxcj;
 public:
-	void set_t() {
-		set_p();
+	friend istream& operator>>(istream& in, student& s) {
+		char name[20];
+		cout << "请输入姓名：";
+		in >> name;
+		s.xm = new char[strlen(name) + 1];
+		strcpy_s(s.xm, strlen(name) + 1, name);		char sex[20];
+		cout << "请输入性别：";
+		in >> sex;
+		s.xb = new char[strlen(sex) + 1];
+		strcpy_s(s.xb, strlen(sex) + 1, sex);
+		cout << "请输入年龄：";
+		in >> s.age;
 		cout << "请输入学号：";
-		cin >> xuehao;
+		cin >> s.xuehao;
 		cout << "请输入班级：";
-		cin >> banji;
-		char zy[20];
+		cin >> s.banji;
 		cout << "请输入专业：";
+		char zy[20];
 		cin >> zy;
-		zhuanye = new char[strlen(zy) + 1];
-		strcpy_s(zhuanye, strlen(zy) + 1, zy);
+		s.zhuanye = new char[strlen(zy) + 1];
+		strcpy_s(s.zhuanye, strlen(zy) + 1, zy);
 		cout << "请输入入学成绩：";
-		cin >> rxcj;
+		cin >> s.rxcj;
+		return in;
 	}
-	void display_s() {
-		display_p();
-		cout << "学号：" << xuehao << "班级：" << banji << "专业：" << zhuanye << "入学成绩：" << rxcj;
+	friend ostream& operator<<(ostream& out, student& s) {
+		out << "姓名:" << s.xm << "性别：" << s.xb << "年龄:" << s.age;
+		out << "学号：" << s.xuehao << "班级：" << s.banji << "专业：" << s.zhuanye << "入学成绩：" << s.rxcj;
+		return out;
 	}
 	~student() {
-		delete[]zhuanye;
+		delete[] zhuanye;
 	}
 };
 int main() {
-	student s;   
-	s.set_t(); 
+	student s;
 
-	s.display_s();
+	cin >> s;
 
+
+	cout << s;
 	return 0;
 
 }
