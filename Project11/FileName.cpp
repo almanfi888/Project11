03c858edc3a0608978908169ab1a0a351c6b5866
almanfi <aa19163278750@outlook.com>
2026-06-05 17:25:56 +0800

    虚继承，参数列表 2026 6 5

--- Project11/FileName.cpp
+++ Project11/FileName.cpp
@@ -1,53 +1,92 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
-class Building {
-protected:
-	int ceng;
-	int fangjian;
+class animal {
 public:
-	Building(int c,int cc):ceng(c),fangjian(cc){}
-};
-class Housing :public Building {
-private:
-	int woshi;
-	int yushi;
-public:
-	Housing(int c, int cc, int w, int ww):Building(c, cc), woshi(w), yushi(ww){}
+	int height;
+	int weight;
+	char* sex;
+	animal() {
+		height = 0; weight = 0;
+		sex = new char[1];
+		sex[0] = '\0';
+	}
 
-	void show() {
-		cout << "层数：" << ceng<< endl;
-		cout << "房间数：" << fangjian<< endl;
-		cout << "卧室数：" << woshi<< endl;
-		cout << "浴室数：" << yushi<< endl;
+	animal(int a, int w, const char* s) {
+		height = a;
+		weight = w;
+		sex = new char[strlen(s) + 1];
+		strcpy_s(sex, strlen(s) + 1, s);
+
 	}
-};
-class Office :public Building {
-private:
-	int miehuo;
-	int phone;
-public:
-	Office(int c,int cc,int m,int mm):Building(c,cc),miehuo(m),phone(mm){}
-	void show() {
-		cout << "层数：" << ceng<< endl;
-		cout << "房间数：" << fangjian<< endl;
-		cout << "灭火器数：" << miehuo<< endl;
-		cout << "电话数：" << phone<< endl;
+	~animal() {
+		delete[] sex;
 	}
+	void show_animal() {
+		cout << "height:" << height << endl;
+		cout << "weight:" << weight << endl;
+		cout << "sex:" << sex << endl;
+	}
+
 };
 
+class ter_animal :virtual public animal {
+public:
+	int running_speed;
+	ter_animal(int a, int w, const char* s, int r) :animal(a, w, s) {
+		running_speed = r;
+
+	}
+	ter_animal()
+	{
+		running_speed = 0;
+	}
+	void show_ter() {
+		show_animal();
+
+		cout << "running_speed:" << running_speed << endl;
+	}
+};
+class aqu_animal :virtual public animal {
+
+public:
+	int swiming_speed;
+	aqu_animal(int a, int w, const char* s, int r) :animal(a, w, s) {
+		swiming_speed = r;
+	}
+	aqu_animal()
+	{
+		swiming_speed = 0;
+	}
+	void show_aqu() {
+		show_animal();
+		cout << "swimming_speed:" << swiming_speed << endl;
+	}
+};
+class amp_animal :public ter_animal, public aqu_animal {
+public:
+	amp_animal(int a, int w, const char* s, int run, int swim) :animal(a, w, s) {
+		running_speed = run;
+		swiming_speed = swim;
+
+	}
+	void show_all() {
+		show_animal();
+
+		cout << "running_speed:" << running_speed << endl;
+		cout << "swimming_speed:" << swiming_speed << endl;
+	}
+};
 int main() {
-	Housing house(6, 12, 8, 4);
-	cout << "===== 住宅楼信息 =====" << endl;
-	house.show();
+	int h, w, run, swim;
+	char sex[10];
+	cin >> h >> w >> sex >> run >> swim;
 
-	cout << endl;
-
-	// 创建办公楼对象
-	Office office(10, 30, 20, 50);
-	cout << "===== 办公楼信息 =====" << endl;
-	office.show();
+	amp_animal amp(h, w, sex, run, swim);
+	amp.show_all();
 
 	return 0;
+
 }
 
 
