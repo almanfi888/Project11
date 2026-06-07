8efd5d2a436cab4d341b7553d13a7b1c8ba81908
almanfi <aa19163278750@outlook.com>
2026-06-07 18:17:48 +0800

    3 虚函数

--- Project11/FileName.cpp
+++ Project11/FileName.cpp
@@ -1,73 +1,100 @@
 ﻿#include <iostream>
 #include <string>
-#include <iomanip>
 using namespace std;
-const double pi = 3.14;
-class shape {
+
+class employee {
+protected:
+	int bh;         
+	string name;    
 public:
-	double x, y;
-	shape() :x(0), y(0) {}
+	employee(int bh0, string name0) :bh(bh0), name(name0) {}
 
 
+	virtual ~employee() {}
+
+
+	virtual double getsalary() = 0;
+	virtual void showinfo() = 0;
 };
-class yuan :public shape {
-private:
-	double r;
+
+
+class manager : public employee {
+protected:
+	double yuexin;
+public:
+	manager(int bh0, string name0, double yuexin0)
+		: employee(bh0, name0), yuexin(yuexin0) {}
+
+	double getsalary() override {
+		return yuexin;
+	}
+
+	void showinfo() override {
+		cout << "【管理人员】" << endl;
+		cout << "编号：" << bh << "\t姓名：" << name << endl;
+		cout << "固定月薪：" << getsalary() << " 元" << endl << endl;
+	}
+};
+
+
+class hourlyworker : public employee {
+protected:
+	double shixin;  
+	int hour;        
 public:
 
-	friend ostream& operator<<(ostream& out, yuan& s) {
-		out << fixed << setprecision(2) << s.perimeter() << " ";
-		return out;
+	hourlyworker(int bh0, string name0, double shixin0, int hour0)
+		: employee(bh0, name0), shixin(shixin0), hour(hour0) {}
+
+
+	double getsalary() override {
+		return shixin * hour;
 	}
 
-	friend istream& operator>>(istream& in, yuan& s) {
-		in >> s.r;
-		return in;
-	}
-
-	double perimeter() {
-		return 2 * pi * r;
+	void showinfo() override {
+		cout << "【计时工人】" << endl;
+		cout << "编号：" << bh << "\t姓名：" << name << endl;
+		cout << "时薪：" << shixin << " 元\t工时：" << hour << " 小时" << endl;
+		cout << "月薪：" << getsalary() << " 元" << endl << endl;
 	}
 };
-class cfx :public shape {
-private:
-	double a, b;
+
+
+class pieceworker : public employee {
+protected:
+	double jiaqian;  
+	int jianshu;   
 public:
-	friend ostream& operator<<(ostream& out, cfx& s) {
-		out << fixed << setprecision(2) << s.perimeter() << " ";
-		return out;
-	}
-	friend istream& operator>>(istream& in, cfx& s) {
-		in >> s.a >> s.b;
-		return in;
+	pieceworker(int bh0, string name0, double jiaqian0, int jianshu0)
+		: employee(bh0, name0), jiaqian(jiaqian0), jianshu(jianshu0) {}
+
+	double getsalary() override {
+		return jiaqian * jianshu;
 	}
 
-	double perimeter() {
-		return 2 * a + 2 * b;
+	void showinfo() override {
+		cout << "【计件工人】" << endl;
+		cout << "编号：" << bh << "\t姓名：" << name << endl;
+		cout << "每件：" << jiaqian << " 元\t件数：" << jianshu << endl;
+		cout << "月薪：" << getsalary() << " 元" << endl << endl;
 	}
+};
 
-};
-class sjx :public shape {
-private:
-	double a, b, c;
-public:
-	friend ostream& operator<<(ostream& out, sjx& s) {
-		out << fixed << setprecision(2) << s.perimeter()<<" ";
-		return out;
-	}
-	friend istream& operator>>(istream& in, sjx& s) {
-		in >> s.a >> s.b >> s.c;
-		return in;
-	}
-	double perimeter() { return a + b + c; }
-};
+
 int main() {
-	yuan a;
-	sjx b;
-	cfx c;
-	cin >> a >> b >> c;
-	cout << a << b << c;
+
+	employee* emp1 = new manager(101, "张三", 8000);
+	employee* emp2 = new hourlyworker(102, "李四", 50, 160);
+	employee* emp3 = new pieceworker(103, "王五", 20, 300);
+
+
+	emp1->showinfo();
+	emp2->showinfo();
+	emp3->showinfo();
+
+	delete emp1;
+	delete emp2;
+	delete emp3;
+
 	return 0;
 }
-
-
