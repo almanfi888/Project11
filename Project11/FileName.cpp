22f2ff656230eb1601c9c63d02710200e3c3f890
almanfi <aa19163278750@outlook.com>
2026-06-12 17:27:41 +0800

    2.3用到了put

--- Project11/FileName.cpp
+++ Project11/FileName.cpp
@@ -7,20 +7,27 @@ using namespace std;
 
 
 int main() {
-	ofstream outFile("stu.txt");
+	ofstream outFile("test.txt");
 	if (!outFile.is_open())
 	{
 		cout << "wenjianchuangjianshibai" << endl;
 		return 1;
 	}
-	string name = "shuai123bi";
-	string id = "1234122133341324";
-	outFile << name << endl;
-	outFile << id << endl;
-	outFile.close();
-	
+	int zhengshu = 1234;
+	double fudian = 88.98;
+	char zifu = 'q';
+	string zifuchuan0 = "almanfi";
+	string zifuchuan1 = "shan shao qi";
+	outFile << zhengshu << endl;
+	outFile << fudian << endl;
+	outFile << zifu << endl;
+	outFile << zifuchuan0 << endl;
 
-	ifstream inFile("stu.txt");
+	outFile << zifuchuan1 << endl;
+	outFile.close();
+
+
+	ifstream inFile("test.txt");
 	if (!inFile.is_open())
 	{
 		cout << "wenjiandakaishibai" << endl;
@@ -28,17 +35,40 @@ int main() {
 
 	}
 
-	string readname, readid;
-	getline(inFile, readname);
-	getline(inFile, readid);
+	outFile.open("new.txt");
+	if (!outFile.is_open()) {
+		cout << "dakaishibai" << endl;
+		return 1;
+	}
+	char ch;
+	while (inFile.get(ch))
+	{
+		outFile.put(ch);
+	}
+	outFile.close();
+	inFile.close();
+
+	inFile.open("new.txt");
+
+	int readi1;
+	double readd;
+	char readc;
+	string read1, read2;
+	inFile >> readi1;
+	inFile >> readd;
+	inFile >> readc;
+	inFile.ignore();
+	getline(inFile, read1);
+	getline(inFile, read2);
 
 	inFile.close();
-	cout << readname;
-	cout << endl << readid << endl;
-
-
-
-	
+	cout << readi1;
+	cout << endl << readd;
+	cout << endl << readc;
+	cout << endl << read1;
+	cout << endl;
+	cout << read2;
+	cout << endl;
 
 	return 0;
 }
